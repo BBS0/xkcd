c576a558af9302b48c22c4f46d7a992e7d62b4f6
Malachite9 <45111844+Malachite9@users.noreply.github.com>
2019-05-18 10:44:20 +1000

    dsa
    asd

--- Assets/Scripts/Spear.cs
+++ Assets/Scripts/Spear.cs
@@ -14,12 +14,17 @@ public class Spear : MonoBehaviour
         spearboiscript = spearboi.GetComponent<SpearThrower>();
         rb = gameObject.GetComponent<Rigidbody>();
         rb.AddForce(transform.forward * (500f * chargelvl));
-
+        StartCoroutine(suicide());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    IEnumerator suicide ()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
     }
 }
